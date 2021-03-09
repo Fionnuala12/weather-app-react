012c5a88d2394c1f602b8e668f907956e3edf0ef
Fionnuala12 <fionnuala.wilson@btinternet.com>
2021-03-09 16:00:18 +0000

    Created WeatherData function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import 'font-awesome/css/font-awesome.min.css';
 import axios from "axios";
-import FormattedDate from './FormattedDate';
+import WeatherData from './WeatherData';
 import './App.css';
 
 function App() {
@@ -19,8 +19,8 @@ function App() {
     maxTemp: response.data.main.temp_max,
     minTemp: response.data.main.temp_min,
     img: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-  })
-};
+  });
+}
 
   if (weatherData.ready) {
   return (
@@ -49,131 +49,25 @@ function App() {
                 </div>
               </div>
             </form>
-            <h1>{weatherData.city}</h1>
-            <h2><FormattedDate date={weatherData.date} /></h2>
-
-            <div className="row">
-              <div className="col-6 mt-4">
-                <div className="clearfix">
-                  <img src={weatherData.img} alt={weatherData.description} className="float-left"/>
-                  <div className="float-left">
-                  <div className="main-weather">
-                    <span className="today-weather-temp">
-                  {Math.round(weatherData.temperature)}
-                    </span>
-                    <span className="units">
-                    <a href="/" className="active">
-                      °C
-                    </a>{" "}
-                    |{" "}
-                    <a href="/" className="inactive">
-                      F°
-                    </a>
-                    </span>
-                    <div className="text-capitalize">
-                      {weatherData.description}
-                    </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-6">
-                <div className="container grid">
-                  <div className="row row-cols-2">
-                    <div className="col box">
-                      {Math.round(weatherData.humidity)}%
-                      <div>
-                        <i className="fas fa-tint"></i> Humidity
-                      </div>
-                    </div>
-                    <div className="col box">
-                      {Math.round(weatherData.wind)}kmh
-                      <div>
-                        <i className="fas fa-wind"></i> Wind
-                      </div>
-                    </div>
-                    <div className="col box">
-                      {Math.round(weatherData.maxTemp)}°C
-                      <div>
-                        <i className="fas fa-thermometer-three-quarters"></i>{" "}
-                        Max Temp
-                      </div>
-                    </div>
-                    <div className="col box">
-                      {Math.round(weatherData.minTemp)}°C
-                      <div>
-                        <i className="fas fa-thermometer-quarter"></i>{" "}
-                        Min. Temp
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <hr />
-            <div class="row weather-forecast">
-                  <div class="col-2"> 
-                  <h4> Sun </h4>
-                  <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="cloudy" />
-                  <div class="weather-forecast-temperature">
-                    <strong> 17°</strong> 15°
-                  </div>
-                  </div>
-                  <div class="col-2"> 
-                  <h4> Mon </h4>
-                  <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="cloudy" />
-                  <div class="weather-forecast-temperature">
-                    <strong> 17°</strong> 15°
-                  </div>
-                  </div>
-                  <div class="col-2"> 
-                  <h4> Tues </h4>
-                  <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="cloudy" />
-                  <div class="weather-forecast-temperature">
-                    <strong> 17°</strong> 15°
-                  </div>
-                  </div>
-                  <div class="col-2"> 
-                  <h4> Wed </h4>
-                  <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="cloudy" />
-                  <div class="weather-forecast-temperature">
-                    <strong> 17°</strong> 15°
-                  </div>
-                  </div>
-                  <div class="col-2"> 
-                  <h4> Thurs </h4>
-                  <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="cloudy" />
-                  <div class="weather-forecast-temperature">
-                    <strong> 17°</strong> 15°
-                  </div>
-                  </div>
-                  <div class="col-2"> 
-                  <h4> Fri </h4>
-                  <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="cloudy" />
-                  <div class="weather-forecast-temperature">
-                    <strong> 17°</strong> 15°
-                  </div>
-                  </div>
-          </div>
-        </div>
-      </div>
-      <small> 
-    <p>
-    <a href="https://github.com/Fionnuala12/weather-app-react">Open-source code</a> by Fionnuala Wilson
-    </p>
+           <WeatherData info={weatherData} />
+           </div> 
+          </div>  
+        </div> 
+        <small> 
+<p>
+<a href="https://github.com/Fionnuala12/weather-app-react">Open-source code</a> by Fionnuala Wilson
+</p>
 </small>
-    </div>
- </div>
-  );
-
-} else {
+      </div>      
+);
+} else { 
   const apiKey = "06c9d19d30f0be8b128071a6b5e0aeb3"; 
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(handleResponse);
   
   return "Loading...."
+  }
+}
 
-}
-}
 
 export default App;
---
+++ src/WeatherData.js
@@ -0,0 +1,114 @@
+import React from "react";
+import FormattedDate from './FormattedDate';
+
+export default function WeatherData(props) {
+    return(
+        <div className="WeatherData">
+       <h1>{props.info.city}</h1>
+        <h2><FormattedDate date={props.info.date} /></h2>
+        <div className="row">
+          <div className="col-6 mt-4">
+            <div className="clearfix">
+              <img src={props.info.img} alt={props.info.description} className="float-left"/>
+              <div className="float-left">
+              <div className="main-weather">
+                <span className="today-weather-temp">
+              {Math.round(props.info.temperature)}
+                </span>
+                <span className="units">
+                <a href="/" className="active">
+                  °C
+                </a>{" "}
+                |{" "}
+                <a href="/" className="inactive">
+                  F°
+                </a>
+                </span>
+                <div className="text-capitalize">
+                  {props.info.description}
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <div className="container grid">
+              <div className="row row-cols-2">
+                <div className="col box">
+                  {Math.round(props.info.humidity)}%
+                  <div>
+                    <i className="fas fa-tint"></i> Humidity
+                  </div>
+                </div>
+                <div className="col box">
+                  {Math.round(props.info.wind)}kmh
+                  <div>
+                    <i className="fas fa-wind"></i> Wind
+                  </div>
+                </div>
+                <div className="col box">
+                  {Math.round(props.info.maxTemp)}°C
+                  <div>
+                    <i className="fas fa-thermometer-three-quarters"></i>{" "}
+                    Max Temp
+                  </div>
+                </div>
+                <div className="col box">
+                  {Math.round(props.info.minTemp)}°C
+                  <div>
+                    <i className="fas fa-thermometer-quarter"></i>{" "}
+                    Min. Temp
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div class="row weather-forecast">
+              <div class="col-2"> 
+              <h4> Sun </h4>
+              <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="cloudy" />
+              <div class="weather-forecast-temperature">
+                <strong> 17°</strong> 15°
+              </div>
+              </div>
+              <div class="col-2"> 
+              <h4> Mon </h4>
+              <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="cloudy" />
+              <div class="weather-forecast-temperature">
+                <strong> 17°</strong> 15°
+              </div>
+              </div>
+              <div class="col-2"> 
+              <h4> Tues </h4>
+              <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="cloudy" />
+              <div class="weather-forecast-temperature">
+                <strong> 17°</strong> 15°
+              </div>
+              </div>
+              <div class="col-2"> 
+              <h4> Wed </h4>
+              <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="cloudy" />
+              <div class="weather-forecast-temperature">
+                <strong> 17°</strong> 15°
+              </div>
+              </div>
+              <div class="col-2"> 
+              <h4> Thurs </h4>
+              <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="cloudy" />
+              <div class="weather-forecast-temperature">
+                <strong> 17°</strong> 15°
+              </div>
+              </div>
+              <div class="col-2"> 
+              <h4> Fri </h4>
+              <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="cloudy" />
+              <div class="weather-forecast-temperature">
+                <strong> 17°</strong> 15°
+              </div>
+              </div>
+      </div>
+    </div>
+    )
+}
